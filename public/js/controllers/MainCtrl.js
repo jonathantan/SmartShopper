a9c81608f7b89d96a96d938877e2284f135577fe
Jonathan Tan <jonathan.tan.13@gmail.com>
2015-11-15 01:24:30 -0500

    commiting for jason to debug

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,42 +1,44 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope, $rootScope, submitService) {
+angular.module('MainCtrl', []).controller('MainController', ['$scope', '$window', function($scope, $window) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
 
 	$scope.myItems = ["table", "chair", "table lamp", "floor mat"];
 
-	$scope.combos = null;
+	$scope.combos = $window.globalVar;
 
-	Window.mycallback = function(data){
-			$rootScope.combos = data;
-			alert('hi');
-	};
-	$('#submit').click(function(){
+	// Window.mycallback = function(data){
+	// 		console.log(data);
+	// 		$rootScope.combos = data;
+	// 		alert('hi');
+	// };
+	// $('#submit').click(function(){
+	// 	submitService.submitForm();
+	// });
 
-		submitService.submitForm();
-		$rootScope.combos = submitService.submitForm();
-	});
 
-	$scope.combos = $rootScope.combos;
-
-}).factory('submitService', function() {
-    return {
-        submitForm : function() {  
-		 	var table = document.getElementById("listItems");
-		 	var budget = document.getElementById("budget").value;
-			listOfItems = [];
-			listOfQty = [];
-			for (var i = 0; i < table.rows.length; i++) {
-				listOfItems.push(table.rows[i].cells[0].innerHTML);
-				listOfQty.push(table.rows[i].cells[1].innerHTML);
-			}
-			//debugger;
-			$.get("http://45.79.136.81:3000/api/combination?categories=" + listOfItems.join() + "&quantity=" + listOfQty.join() + "&price=" + budget, function(data, status){
-				console.log(data);
-			//	debugger;
+}]);
+// .factory('submitService', function() {
+//     return {
+//         submitForm : function() {  
+// 		 	var table = document.getElementById("listItems");
+// 		 	var budget = document.getElementById("budget").value;
+// 			listOfItems = [];
+// 			listOfQty = [];
+// 			for (var i = 0; i < table.rows.length; i++) {
+// 				listOfItems.push(table.rows[i].cells[0].innerHTML);
+// 				listOfQty.push(table.rows[i].cells[1].innerHTML);
+// 			}
+// 			var callback = function (data) {
+// 				return data;
+// 			}
+// 			$.get("http://45.79.136.81:3000/api/combination?categories=" + listOfItems.join() + "&quantity=" + listOfQty.join() + "&price=" + budget, function(data, status){
+// 				console.log(data);
+// 				//	debugger;
 				
-				Window.mycallback(data);
-				//return data;
-			});
-		}
-    };
-});;
+// 				// Window.mycallback(data);
+// 				globalVar = data;
+// 				return data;
+// 			});
+// 		}
+//     };
+// });
